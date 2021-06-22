7c9d75575a1a3d26f63840a713cea586145e63e2
Tatooine-Soldier <58118455+Tatooine-Soldier@users.noreply.github.com>
2021-06-22 12:14:23 +0100

    Add files via upload
    Designed the contact page with a text area to write comments. Social media logos also implemented.

---
+++ src/components/USA.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function USA() {
+    return (
+        <>
+            <div className="grid-pic-container">
+                    {country}
+                </div>
+                <div className="grid-pic-container">
+                    {country}
+                </div>
+                <div className="grid-pic-container">
+                    {country}
+                </div>
+                <div className="grid-pic-container">
+                    {country}
+                </div>
+        </>
+    )
+}
